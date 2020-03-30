0ea4e1c27c517d23f745d5e045a81ebfc51ea8a0
Sergey Garin <60073410@leroymerlin.ru>
2020-03-30 14:06:22 +0300

    bugfix

--- src/reducer.ts
+++ src/reducer.ts
@@ -4,7 +4,7 @@ import { updateCookie, REDUX_COOKIES_UPDATE } from './actions';
 export function createReducer(cookies: string) {
   const cookieWorker = new Cookies(cookies);
 
-  return function reducer(state = cookieWorker.getAll(), action: ReturnType<typeof updateCookie>) {
+  return function reducer(state = getState(cookieWorker), action: ReturnType<typeof updateCookie>) {
     if (action.type === REDUX_COOKIES_UPDATE) {
       if (action.payload.value === undefined) {
         cookieWorker.remove(action.payload.name);
@@ -12,14 +12,18 @@ export function createReducer(cookies: string) {
         cookieWorker.set(action.payload.name, action.payload.value, action.payload.options);
       }
 
-      return {
-        cookieString: cookieWorker.getAll({ doNotParse: true }) as string,
-        cookies: {
-          ...(cookieWorker.getAll() as Record<string, string>),
-        },
-      };
+      return getState(cookieWorker);
     }
 
     return state;
+  };
+}
+
+function getState(cookieWorker: Cookies) {
+  return {
+    cookieString: cookieWorker.getAll({ doNotParse: true }) as string,
+    cookies: {
+      ...(cookieWorker.getAll() as Record<string, string>),
+    },
   };
 }
